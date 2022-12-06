079a3134e1cbf32c107e2ee190fd28275a84b9a2
Sebastian Markestedt <sebastian.markestedt@consafelogistics.com>
2022-12-06 08:08:05 +0100

    Day 6

---
+++ day06/day06.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/markestedt/advent-of-code-2022/utils"
+)
+
+func main() {
+	input := utils.GetLines("day06.txt")[0]
+	start := time.Now()
+
+	part1 := solve(input, 4)
+	part2 := solve(input, 14)
+
+	timeElapsed := time.Since(start)
+	log.Printf("Part1: %d. Part2: %d. Took: %dms", part1, part2, timeElapsed.Milliseconds())
+}
+
+func solve(input string, markerSize int) int {
+	var answer int
+
+	for i := markerSize; i < len(input); i++ {
+		marker := input[i-markerSize : i]
+
+		if !hasDuplicate(marker) {
+			answer = i
+			break
+		}
+	}
+	return answer
+}
+
+func hasDuplicate(input string) bool {
+	var visited []string
+
+	for _, s := range input {
+		if utils.Contains(visited, string(s)) {
+			return true
+		}
+		visited = append(visited, string(s))
+	}
+	return false
+}
