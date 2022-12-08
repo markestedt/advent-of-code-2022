72181cbfd0a02bb90e95feeff07f4dcc670c62ed
Sebastian Markestedt <sebastian.markestedt@consafelogistics.com>
2022-12-08 14:24:18 +0100

    Refactor Day 8 Part 1

--- day08/day08.go
+++ day08/day08.go
@@ -38,40 +38,94 @@ func getRow(m map[utils.Point]int, y int) []utils.Point {
 }
 
 func main() {
-	lines := utils.GetLines("day08/day08test.txt")
-	grid := utils.BuildGrid(lines)
+	lines := utils.GetLines("day08/day08.txt")
+	log.Println(part1(lines))
 
-	part1 := 0
-	part2 := 0
+	grid := utils.BuildGrid(lines)
+	answerPart2 := 0
 
 	for p, _ := range grid {
-		score, visible := solve(grid, p)
+		score := part2(grid, p)
 
-		if visible {
-			part1++
+		if score > answerPart2 {
+			answerPart2 = score
 		}
-
-		if score > part2 {
-			part2 = score
-		}
-
 	}
-
-	log.Println(part1)
-	log.Println(part2)
+	log.Println(answerPart2)
 }
 
-func solve(forest map[utils.Point]int, tree utils.Point) (int, bool) {
+func part1(lines []string) int {
+	var visibleTrees []utils.Point
+
+	//left to right
+	for i, line := range lines {
+		highestInLine := -1
+		for j, t := range line {
+			val := int(t - '0')
+			if val > highestInLine {
+				highestInLine = val
+				point := utils.Point{Y: i, X: j}
+				if !utils.Contains(visibleTrees, point) {
+					visibleTrees = append(visibleTrees, point)
+				}
+			}
+		}
+	}
+
+	//right to left
+	for i, line := range lines {
+		highestInLine := -1
+		for j := len(line) - 1; j >= 0; j-- {
+			val := int(line[j] - '0')
+			if val > highestInLine {
+				highestInLine = val
+				point := utils.Point{Y: i, X: j}
+				if !utils.Contains(visibleTrees, point) {
+					visibleTrees = append(visibleTrees, point)
+				}
+			}
+		}
+	}
+
+	//top to bottom
+	for i := 0; i < len(lines); i++ {
+		highestInCol := -1
+		for j := 0; j < len(lines); j++ {
+			val := int(lines[j][i] - '0')
+			if val > highestInCol {
+				highestInCol = val
+				point := utils.Point{Y: j, X: i}
+				if !utils.Contains(visibleTrees, point) {
+					visibleTrees = append(visibleTrees, point)
+				}
+			}
+		}
+	}
+
+	//bottom to top
+	for i := 0; i < len(lines); i++ {
+		highestInCol := -1
+		for j := len(lines) - 1; j >= 0; j-- {
+			val := int(lines[j][i] - '0')
+			if val > highestInCol {
+				highestInCol = val
+				point := utils.Point{Y: j, X: i}
+				if !utils.Contains(visibleTrees, point) {
+					visibleTrees = append(visibleTrees, point)
+				}
+			}
+		}
+	}
+
+	return len(visibleTrees)
+}
+
+func part2(forest map[utils.Point]int, tree utils.Point) int {
 	currentTree := forest[tree]
 	scoreLeft := 0
 	scoreRight := 0
 	scoreUp := 0
 	scoreDown := 0
-
-	visibleLeft := true
-	visibleRight := true
-	visibleUp := true
-	visibleDown := true
 
 	row := getRow(forest, tree.Y)
 	rowBefore := row[:tree.X]
@@ -85,7 +139,6 @@ func solve(forest map[utils.Point]int, tree utils.Point) (int, bool) {
 		nextTree := forest[rowBefore[i]]
 		scoreLeft++
 		if nextTree >= currentTree {
-			visibleLeft = false
 			break
 		}
 	}
@@ -93,7 +146,6 @@ func solve(forest map[utils.Point]int, tree utils.Point) (int, bool) {
 		nextTree := forest[rowAfter[i]]
 		scoreRight++
 		if nextTree >= currentTree {
-			visibleRight = false
 			break
 		}
 	}
@@ -102,7 +154,6 @@ func solve(forest map[utils.Point]int, tree utils.Point) (int, bool) {
 		nextTree := forest[colBefore[i]]
 		scoreUp++
 		if nextTree >= currentTree {
-			visibleUp = false
 			break
 		}
 	}
@@ -110,10 +161,9 @@ func solve(forest map[utils.Point]int, tree utils.Point) (int, bool) {
 		nextTree := forest[colAfter[i]]
 		scoreDown++
 		if nextTree >= currentTree {
-			visibleDown = false
 			break
 		}
 	}
-	visible := visibleLeft || visibleRight || visibleUp || visibleDown
-	return scoreLeft * scoreRight * scoreUp * scoreDown, visible
+
+	return scoreLeft * scoreRight * scoreUp * scoreDown
 }
